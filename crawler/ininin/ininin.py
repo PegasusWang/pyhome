3f2ac4f559bfc4418ec3cc5280eb480be322aeee
PegasusWang <291374108@qq.com>
2016-07-03 16:12:50 +0800

    ad

--- crawler/ininin/ininin.py
+++ crawler/ininin/ininin.py
@@ -1,6 +1,17 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import _env
+import requests
+
+
+URL = 'http://www.ininin.com/'
+CURL_STR
+
+
+def get_category(URL):
+
+
 
 if __name__ == '__main__':
     pass
