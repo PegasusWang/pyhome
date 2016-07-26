ae36189a39f047a1215f21c5201fe059b0c6726e
PegasusWang <291374108@qq.com>
2016-07-26 09:54:57 +0800

    add func cookie string to dict

--- crawler/web_util.py
+++ crawler/web_util.py
@@ -29,7 +29,7 @@ except ImportError:    # py2
     from urllib2 import urlopen
 try:
     from http.cookies import SimpleCookie
-except ImportError:    # py2
+except ImportError:
     from Cookie import SimpleCookie
 
 
@@ -262,12 +262,20 @@ def logged(class_):
 
 
 def cookie_dict_from_cookie_str(cookie_str):
+    """cookie_dict_from_str Cookie字符串返回成dict
+
+    :param cookie_str: cookies string
+    """
+    cookie = SimpleCookie()
+    cookie.load(cookie_str)
+    return {key: morsel.value for key, morsel in cookie.items()}
+
+
+def cookie_dict_from_response(r):
     """cookie_dict_from_response 获取返回的response对象的Set-Cookie字符串
     并返回成dict
 
     :param r: requests.models.Response
     """
-    # cookie_str = r.headers.get('Set-Cookie')
-    cookie = SimpleCookie()
-    cookie.load(cookie_str)
-    return {key: morsel.value for key, morsel in cookie.items()}
+    cookie_str = r.headers.get('Set-Cookie')
+    return cookie_dict_from_cookie_str(cookie_str)
