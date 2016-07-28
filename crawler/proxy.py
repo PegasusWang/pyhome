1a543736ac5abe5f54a172065ec0fac7c30d2ce6
PegasusWang <291374108@qq.com>
2016-07-28 10:08:38 +0800

    add timeout

--- crawler/proxy.py
+++ crawler/proxy.py
@@ -4,6 +4,7 @@
 
 import _env
 import re
+import concurrent.futures
 from io import open
 from collections import namedtuple
 from pprint import pprint
@@ -117,34 +118,36 @@ class XiciCrawler(ThreadPoolCrawler):
             self.bulk_update_to_mongo(ip_info_dict_yield)
         elif response.status_code == 503:
             change_ip()
+            self.urls.append(url)    # retry
 
 
+@logged
 class CheckXiciCralwer(ThreadPoolCrawler):
     """CheckXiciCralwer 用来测试代理的有效性，及时剔除没用的代理"""
 
     db = get_db('htmldb')
     col = getattr(db, 'xici_proxy')    # collection
-    timeout = 10    # 测试超时时间
+    timeout = 20    # 测试超时时间
 
     def init_urls(self):
         """init_urls get all ip proxy from monggo"""
         url = 'http://www.baidu.com'
-        for ip_info in self.col.find(no_cursor_timeout=True):
+        for ip_info in self.col.find(no_cursor_timeout=True)[:100]:
             ip, port = ip_info['ip'], ip_info['port']
             if ip and port:
-                self.urls.append(url, ip, port)
+                self.urls.append((url, ip, port))    # tuple
 
     def run_async(self):
-        for url_list in chunks(self.urls, 100):
+        for url_list in chunks(self.urls, 2):    # handle 100 every times
             with concurrent.futures.ThreadPoolExecutor(max_workers=self.concurrency) as executor:
                 future_to_url = {
                     executor.submit(
-                        self.get, url, proxies=get_proxy_dict(ip, int(port))
-                    ): url
+                        self.get, url, proxies=get_proxy_dict(ip, int(port)), timeout=self.timeout
+                    ): (url, ip, port)
                     for (url, ip, port) in url_list
                 }
                 for future in concurrent.futures.as_completed(future_to_url):
-                    url = future_to_url[future]
+                    url, ip, port = future_to_url[future]
                     try:
                         response = future.result()
                     except Exception as e:
@@ -153,11 +156,12 @@ class CheckXiciCralwer(ThreadPoolCrawler):
                         print(e)
                     else:
                         self.handle_response(url, response)
-
-
+            break    # TODO  remoe after test success
 
     def handle_response(self, url, response):
         """handle_response 验证代理的合法性。通过发送简单请求检测是否超时"""
+        if response:
+            self.logger.info('url: %s', url, response.status_code)
 
 
 def test():
@@ -171,10 +175,15 @@ def test():
             print(i)
 
 
-def main():
+def run_xici():
     xici_crawler = XiciCrawler()
     xici_crawler.run(use_thread=False)
 
+
+def check_proxy():
+    c = CheckXiciCralwer()
+    c.run()
+
+
 if __name__ == '__main__':
-    # test()
-    main()
+    check_proxy()
