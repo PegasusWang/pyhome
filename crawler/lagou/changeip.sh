ad6f1e7b9028957bc465d7467005b07049e5b69e
PegasusWang <291374108@qq.com>
2016-07-29 15:05:52 +0800

    modify changeip

--- crawler/lagou/changeip.sh
+++ crawler/lagou/changeip.sh
@@ -2,4 +2,7 @@
 
 curl --socks5 127.0.01:9050 http://checkip.amazonaws.com/
 (echo authenticate '"rentonggeji"'; echo signal newnym; echo quit) | nc localhost 9051
+
+
+sleep 3
 curl --socks5 127.0.01:9050 http://checkip.amazonaws.com/
