0a66b5b18a2d4dab290d9a667e9eeac451c94369
PegasusWang <291374108@qq.com>
2016-08-07 18:19:47 +0800

    add handle doc

--- crawler/lagou/handle_db.py
+++ crawler/lagou/handle_db.py
@@ -2,15 +2,19 @@
 # -*- coding: utf-8 -*-
 
 import _env
-from lib._db import get_db
 from pprint import pprint as pp
+
+from lib._db import get_db
 from html_parser import Bs4HtmlParser
+from spider import LagouCrawler
+
+
 db = get_db('htmldb')
 col = getattr(db, 'lagou_html')    # collection
 
 
 def test_get_db():
-    o = col.find_one({'_id':1234})
+    o = col.find_one({'_id': 1234})
     html = o['html']
     # print html
     pp(o)
@@ -24,7 +28,16 @@ def test_get_html():
     print(p.html)
     t = p.bs.find('p', class_='msg')
     text = t.get_text()
-    print text
+    print(text)
+
+
+def count_how_many_block_html():
+    cnt = 0
+    for html_doc in col.find(snapshot=True):
+        html = html_doc['html']
+        if LagouCrawler.is_block_html(html):
+            cnt += 1
+    return cnt
 
 
 if __name__ == '__main__':
