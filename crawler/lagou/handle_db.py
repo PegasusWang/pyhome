706afc1f5c4b90cfa9cd542122c8dc8c1fcfd094
PegasusWang <291374108@qq.com>
2016-08-21 11:41:25 +0800

    add handle db

--- crawler/lagou/handle_db.py
+++ crawler/lagou/handle_db.py
@@ -3,14 +3,17 @@
 
 import _env
 from pprint import pprint as pp
-
-from lib._db import get_db
+from tornado.util import ObjectDict
+from lib._db import get_db, redis_client as r
 from html_parser import Bs4HtmlParser
+from lagou_parse import LagouHtmlParser
 from spider import LagouCrawler
+from web_util import logged
 
 
 db = get_db('htmldb')
-col = getattr(db, 'lagou_html')    # collection
+lagou_html_col = getattr(db, 'lagou_html')    # collection
+lagou_job_col = getattr(db, 'lagou_job_col')
 
 
 def test_get_db():
@@ -54,8 +57,45 @@ def count_how_many_check_html():
     # col.remove({'_id':{'$in':_id_list}})
 
 
+@logged
+class ParseJob(object):
+    """用来处理抓下来的html页面，把需要的数据从html中提取出来单独存储"""
+
+    db = get_db('htmldb')
+
+    def __init__(self):
+        self.from_col = getattr(self.db, 'lagou_html')
+        self.to_col = getattr(self.db, 'lagou_job')
+        self.key = self.__class__.__name__
+        self.last_id = int(r.get(self.key)) or 0
+
+    def run_job(self):
+        """lagou job页面的信息任务"""
+        for doc_dict in self.col.find({'_id': {'$gte': self.last_id}}):
+            if 'job' in doc_dict['url']:
+                doc = ObjectDict(doc_dict)
+                assert doc_dict.url and doc.html
+                job_parser = LagouHtmlParser(doc.url, doc.html)
+                data_dict = job_parser.parse_job()
+                self.logger.info(
+                    'handle url: %s %s:%s',
+                    doc.url, data_dict['source'], data_dict['job_name']
+                )
+                self.to_col.update(
+                    {
+                        '_id': doc._id,
+                    },
+                    {
+                        '$set': data_dict
+                    },
+                    upsert=True
+                )
+
+
 if __name__ == '__main__':
     # test_get_html()
     # print(count_how_many_block_html())
     # print(count_how_many_check_html())
-    print(col.count())
+    # print(col.count())
+    p = ParseJob()
+    p.run_job()
---
+++ crawler/lagou/lagou_parser.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+
+import _env
+import json
+from pprint import pformat
+from six import import string_types
+from bs4 import BeautifulSoup
+from html_parser import Bs4HtmlParser
+from lib._db import get_db
+
+
+class LagouHtmlParser(Bs4HtmlParser):
+    """HtmlParser 解析lagou网页内容"""
+
+    def parse(self):
+        if 'gongsi' in self.url:
+            self.parse_gongsi()
+        elif 'job' in self.url:
+            self.parse_job()
+
+    def parse_gongsi(self):
+        """parse_gongsi"""
+        raise NotImplementedError
+
+    def parse_job(self):
+        """parse_job
+        :return: dict for lagou job html data
+        """
+        bs = self.bs
+        assert 'job' in self.url
+        job_detail_dl_tag = bs.find(class_='job_detail')
+        source = job_detail_dl_tag.find('div').text    # 盒子鱼英语技术部招聘
+        job = job_detail_dl_tag.find('h1').get('title')  # 数据挖掘
+        job_request_dd_tag = job_detail_dl_tag.find('dd', class_="job_request")
+        salary, work_place, work_experience, work_edu, work_time = [
+            span.text.strip() for span in job_request_dd_tag.find_all('span')
+        ]
+        job_attract = job_request_dd_tag.find_all('p')[1].text
+        # publist_date = job_request_dd_tag.find_all('p')[1].text
+        job_description_dd_tag = bs.find('dd', class_='job_bt')
+        job_description = job_description_dd_tag.text
+
+        job_company_dl_tag = bs.find('dl', class_='job_company')
+        company_lagou_url = job_company_dl_tag.find('a').get('href')
+        company_name = job_company_dl_tag.find('img').get('alt')
+
+        ul_feature_tags = bs.find_all('ul', class_='c_feature')
+        ul_feature_li_tag_list = ul_feature_tags[0].find_all('li')
+        company_fields, company_people, company_url = [
+            ul_tag.text.split()[1] for ul_tag in ul_feature_li_tag_list
+        ]
+
+        company_step = ul_feature_tags[1].find('li').text.split()[1]
+        company_addr_text = bs.find('div', class_='work_addr').text
+        company_addr = ''.join(company_addr_text.split())    # remove blank
+
+        to_save_set = frozenset([
+            'source', 'job', 'salary', 'work_place', 'work_experience',
+            'work_edu', 'work_time', 'job_attract', 'job_description',
+            'company_lagou_url', 'company_name', 'company_fields',
+            'company_people', 'company_url', 'company_step', 'company_addr',
+        ])
+        data = {}
+        for k, v in locals().items():
+            if k in to_save_set:
+                if isinstance(k, string_types):
+                    data[k] = v.strip()
+                else:
+                    data[k] = v
+        data['job_url'] = self.url
+
+        return data
+
+
+if __name__ == '__main__':
+    with open('./t.html') as f:
+        p = LagouHtmlParser(url='http://job', html=f.read())
+        data = p.parse_job()
+        import pprint
+        pprint.pprint(data)
